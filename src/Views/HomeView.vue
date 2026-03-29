ae11869eaa40960f4c50e88deabb8046534b74d1
Clement <clementmwangi520@gmail.com>
2026-03-29 18:24:03 +0300

    created vue app and accessed data manually without server

---
+++ src/Views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+    import Hero from '@/components/Hero.vue';
+    import Homecards from '@/components/Homecards.vue';
+    import Joblistings from '@/components/Joblistings.vue';
+</script>
+
+<template>
+    <Hero />
+    <Homecards />
+    <Joblistings :limit="4" :showButton="true" />
+</template>
+  
+  
